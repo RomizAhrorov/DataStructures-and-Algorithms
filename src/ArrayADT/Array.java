9a3949f6d1c1319e0017d93833486fb3367255bb
Romiz Ahrorov <romizhonahrori@gmail.com>
2024-01-22 11:20:55 +0500

    New methods included to Array Class
    Array Data Structure was enhanced by including more neccessary functionality like
    shiftLeft(), shiftRight(), rotateLeft(), rotateRight(), sum(), average(), isSorted(), Append(int[] arr)

--- src/ArrayADT/Array.java
+++ src/ArrayADT/Array.java
@@ -1,19 +1,21 @@
 package ArrayADT;
 
-public class Array<T extends Comparable<T>> {
-    private T[] array;
+import java.awt.*;
+
+public class Array {
+    private int[] array;
     private final int size;
     private int length;
 
     private void swap(int index1, int index2) {
-        T temp = this.array[index1];
+        int temp = this.array[index1];
         this.array[index1] = this.array[index2];
         this.array[index2] = temp;
     }
 
     public Array() {
         this.size = 100;
-        this.array = (T[]) new Comparable[this.size];
+        this.array = new int[this.size];
         this.length = 0;
     }
 
@@ -22,7 +24,7 @@ public class Array<T extends Comparable<T>> {
             throw new IllegalArgumentException("Initial capacity cannot be negative: " + size);
 
         this.size = size;
-        this.array = (T[]) new Comparable[this.size];
+        this.array = new int[this.size];
         this.length = 0;
     }
 
@@ -34,12 +36,12 @@ public class Array<T extends Comparable<T>> {
         }
     }
 
-    public void append(T newValue) {
+    public void append(int newValue) {
         this.array[this.length] = newValue;
         this.length++;
     }
 
-    public void insert(int index, T newValue) {
+    public void insert(int index, int newValue) {
         if(index < 0 || index > this.length)
             throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for length " + this.length);
 
@@ -50,11 +52,11 @@ public class Array<T extends Comparable<T>> {
         this.length++;
     }
 
-    public T delete(int index) {
+    public int delete(int index) {
         if(index < 0 || index >= this.length)
             throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for length " + this.length);
 
-        T deletedValue = this.array[index];
+        int deletedValue = this.array[index];
         for(int i = index; i < this.length - 1; i++) {
             this.array[i] = this.array[i+1];
         }
@@ -63,7 +65,7 @@ public class Array<T extends Comparable<T>> {
         return deletedValue;
     }
 
-    public int searchLinear(T searchedValue) {
+    public int searchLinear(int searchedValue) {
         int foundIndex = -1;
         for(int i = 0; i < this.length; i++) {
             if(searchedValue == this.array[i]) {
@@ -75,15 +77,14 @@ public class Array<T extends Comparable<T>> {
         return foundIndex;
     }
 
-    public int searchBinary(T searchedValue) {
-        int low = 0, high = this.length - 1, mid, comparison;
+    public int searchBinary(int searchedValue) {
+        int low = 0, high = this.length - 1, mid;
 
         while (low <= high) {
             mid = (low + high) / 2;
-            comparison = searchedValue.compareTo(this.array[mid]);
-            if(comparison == 0)
+            if (searchedValue == this.array[mid])
                 return mid;
-            else if (comparison > 0)
+            else if (searchedValue > this.array[mid])
                 low = mid + 1;
             else
                 high = mid - 1;
@@ -92,36 +93,32 @@ public class Array<T extends Comparable<T>> {
         return -1;
     }
 
-    public T get(int index) {
+    public int get(int index) {
         if(index < 0 || index >= this.length)
             throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for length " + this.length);
         return this.array[index];
     }
 
-    public void set(int index, T updatedValue) {
+    public void set(int index, int updatedValue) {
         if(index < 0 || index >= this.length)
             throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for length " + this.length);
         this.array[index] = updatedValue;
     }
 
-    public T max() {
-        T maxValue = this.array[0];
-        int comparison;
+    public int max() {
+        int maxValue = this.array[0];
         for(int i = 0; i < this.length; i++) {
-            comparison = this.array[i].compareTo(maxValue);
-            if(comparison > 0)
+            if(this.array[i] > maxValue)
                 maxValue = this.array[i];
         }
 
         return maxValue;
     }
 
-    public T min() {
-        T minValue = this.array[0];
-        int comparison;
+    public int min() {
+        int minValue = this.array[0];
         for(int i = 0; i < this.length; i++) {
-            comparison = this.array[i].compareTo(minValue);
-            if(comparison < 0)
+            if(this.array[i] < minValue)
                 minValue = this.array[i];
         }
 
@@ -130,10 +127,98 @@ public class Array<T extends Comparable<T>> {
 
     public void reverse() {
         int i = 0, j = this.length - 1;
-        while(i < j) {
+        while(i < j)
             swap(i++, j--);
+    }
+
+    public void shiftLeft(int shiftCount) {
+        if (shiftCount > this.length) {
+            throw new IllegalArgumentException("The number of shifts cannot be greater than the length of the array");
+        }
+        for(int i = 0; i < this.length; i++) {
+            if(i >= this.length - shiftCount)
+                this.array[i] = 0;
+            else
+                this.array[i] = this.array[i+shiftCount];
         }
     }
 
+    public void shiftRight(int shiftCount) {
+        if (shiftCount > this.length) {
+            throw new IllegalArgumentException("The number of shifts cannot be greater than the length of the array");
+        }
+        for(int i = this.length - 1; i >= 0; i--) {
+            if(i < shiftCount)
+                this.array[i] = 0;
+            else
+                this.array[i] = this.array[i-shiftCount];
+        }
+    }
 
+    public void rotateLeft(int rotateCount) {
+        int rotates = rotateCount % this.length;
+        int[] temp = new int[this.length];
+        for(int i = 0; i < this.length; i++) {
+            if(i >= this.length - rotates)
+                temp[i] = this.array[i+rotates-this.length];
+            else
+                temp[i] = this.array[i+rotates];
+        }
+        System.arraycopy(temp, 0, this.array, 0, this.length);
+    }
+
+    public void rotateRight(int rotateCount) {
+        int rotates = rotateCount % this.length;
+        int[] temp = new int[this.length];
+        for(int i = this.length - 1; i >= 0; i--) {
+            if(i < rotates)
+                temp[i] = this.array[i-rotates+this.length];
+            else
+                temp[i] = this.array[i-rotates];
+        }
+        System.arraycopy(temp, 0, this.array, 0, this.length);
+    }
+
+    public int sum() {
+        int sumValue = 0;
+        for(int i = 0; i < this.length; i++)
+            sumValue += this.array[i];
+
+        return sumValue;
+    }
+
+    public float average() {
+        return (float) this.sum() / (float) this.length;
+    }
+
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    public boolean isSorted() {
+        if(this.isEmpty() || this.length == 1) {
+            return true;
+        }
+
+        boolean isAscending = this.array[0] < this.array[1];
+
+        for(int i = 0; i < this.length - 1; i++) {
+            if(isAscending && this.array[i] > this.array[i+1]) {
+                return false;
+            }
+            if(!isAscending && this.array[i] < this.array[i+1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void append(int[] arr, int length) {
+        if(this.length + length > this.size)
+            throw new IllegalArgumentException("There is no enough space in the array to append given array.");
+
+        if (length >= 0) System.arraycopy(arr, 0, this.array, this.length, length);
+        this.length += length;
+    }
 }
